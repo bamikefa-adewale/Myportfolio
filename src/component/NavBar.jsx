a0643f925739a0f2b15d0bab9115435a55d62b7a
bamikefa-adewale <bamikefaadewale@gmail.com>
2025-03-04 00:15:03 +0100

    stickly added to navbar

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -17,45 +17,47 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="flex flex-wrap justify-between py-5 font-outfit text-lg mb-10 mx-auto w-[90%] items-center">
-      <h6 className=" italic  text-fuchsia-500 rounded-md px-4 py-2 text-3xl font-semibold">
-        Adew<span className="text-white">ale</span>
-      </h6>
+    <nav className="sticky top-0 z-50 backdrop-blur-xl bg-black/30">
+      <div className="flex flex-wrap mx-auto w-[90%] justify-between py-3 font-outfit text-lg mb-10 items-center">
+        <h6 className=" italic  text-fuchsia-500 rounded-md px-4 py-2 text-3xl font-semibold">
+          Adew<span className="text-white">ale</span>
+        </h6>
 
-      {/* Hamburger Icon */}
-      <button
-        onClick={() => setIsMenuOpen(true)}
-        className="text-white text-2xl lg:hidden"
-      >
-        <FaBars />
-      </button>
-      {/* Desktop Menu */}
-      <div className="hidden md:flex gap-80 items-center">
-        <NavMenu
-          navitems={navitems}
-          activeItem={activeItem}
-          setActiveItem={setActiveItem}
-          handleScroll={handleScroll}
-        />
-        <a
-          href="https://wa.me/2348166176457"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="py-[10px] sm:py-[20px] capitalize px-[30px] sm:px-[50px] cursor-pointer rounded-full bg-gradient-to-r text-white from-purple-500 to-pink-500 hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out"
+        {/* Hamburger Icon */}
+        <button
+          onClick={() => setIsMenuOpen(true)}
+          className="text-white text-2xl lg:hidden"
         >
-          Contact Me
-        </a>
+          <FaBars />
+        </button>
+        {/* Desktop Menu */}
+        <div className="hidden md:flex gap-80 items-center">
+          <NavMenu
+            navitems={navitems}
+            activeItem={activeItem}
+            setActiveItem={setActiveItem}
+            handleScroll={handleScroll}
+          />
+          <a
+            href="https://wa.me/2348166176457"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="py-[10px] sm:py-[20px] capitalize px-[30px] sm:px-[50px] cursor-pointer rounded-full bg-gradient-to-r text-white from-purple-500 to-pink-500 hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out"
+          >
+            Contact Me
+          </a>
+        </div>
+        {/* Mobile Menu Modal */}
+        {isMenuOpen && (
+          <MobileMenuModal
+            navitems={navitems}
+            activeItem={activeItem}
+            setActiveItem={setActiveItem}
+            handleScroll={handleScroll}
+            onClose={() => setIsMenuOpen(false)}
+          />
+        )}
       </div>
-      {/* Mobile Menu Modal */}
-      {isMenuOpen && (
-        <MobileMenuModal
-          navitems={navitems}
-          activeItem={activeItem}
-          setActiveItem={setActiveItem}
-          handleScroll={handleScroll}
-          onClose={() => setIsMenuOpen(false)}
-        />
-      )}
     </nav>
   );
 };
