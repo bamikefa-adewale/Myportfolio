c10c50b98e305190de9c8300236daf7b891bdfb5
bamikefa-adewale <bamikefaadewale@gmail.com>
2025-04-08 18:22:33 +0100

    footer comment

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,19 +1,18 @@
-
-
 const Footer = () => {
   return (
     <section className=" mx-5 sm:mx-10 lg:mx-40 flex flex-col text-white gap-8">
       <div className="my-10">
         <div className="flex justify-between text-center gap-10 lg:gap-96 flex-col lg:flex-row items-center lg:items-start">
-          
           <h6 className=" text-center italic  text-fuchsia-500 rounded-md px-4 py-2 text-3xl font-semibold">
             Adew<span className="text-white">ale</span>
           </h6>
           <p className="text-white text-base font-normal sm:text-lg max-w-[400px] mx-auto lg:mx-0">
-            I am a Frontend Developer skilled in HTML, CSS, React.js, Next.js,
-            TypeScript, and Tailwind CSS, with experience in building responsive
-            and user-friendly web applications. I also have expertise in
-            Node.js, PostgreSQL, Git, and GitHub.
+            I am aExperienced in building modern, scalable web applications
+            using React.js, Next.js, TypeScript, and Tailwind CSS on the
+            frontend. Skilled in backend development with Node.js, NestJS,
+            PostgreSQL, and TypeORM. Proficient in version control with Git and
+            GitHub, with a strong focus on responsive design and user-friendly
+            interfaces.
           </p>
         </div>
         <hr className="my-5" />
